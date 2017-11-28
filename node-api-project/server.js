eadcaa79e490c132650624ab16685a353d9f2193
vishwesh <vishweshsoni@gmail.com>
2017-11-28 13:18:00 +0530

    getting user server.js modified

--- node-api-project/server.js
+++ node-api-project/server.js
@@ -8,5 +8,24 @@ var connection = mysql.createConnection({ // Mysql Connection
     password : '',
     database : 'login1',
 });
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Body parser use JSON data
+app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+        });
+app.get('/users',function(req,res) {
+    var data={
+        error:1,
+        "Users":""
+    };
+    connection.query("SELECT * FROM log",function (err,rows,fields) {
+    if(rows.length!=0){
+         data["error"]=0;
+          data["Users"]=rows;
+          res.json(data);
+        }else{
+           data["Users"]='no users found ..';
+           res.json(data);
+        }
+    });
+});
