fc7f3e4ac282ea8681a55aba32515b8997144356
vishwesh <vishweshsoni@gmail.com>
2017-11-29 17:15:43 +0530

    --adding class for signup

---
+++ android/app/src/main/java/Httpcall.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by vishw on 29-11-2017.
+ */
+
+public class Httpcall {
+}
---
+++ android/app/src/main/java/com/hrm/vishwesh/hrms/BackgroundSignup.java
@@ -0,0 +1,8 @@
+package com.hrm.vishwesh.hrms;
+
+/**
+ * Created by vishw on 29-11-2017.
+ */
+
+public class BackgroundSignup {
+}
